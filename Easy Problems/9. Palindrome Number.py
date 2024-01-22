0461720d88458383eee7c3baa2b15704efe98aa1
khaled_Omar <kmr444166@gmail.com>
2024-01-22 13:35:41 +0200

    delete the old files

--- Easy Problems/9. Palindrome Number.py
+++
@@ -1,7 +0,0 @@
-#want to know if the number is a palindrome
-class Solution(object):
-    def isPalindrome(self, x):
-       x_str =str(x)
-       if x_str == x_str[::-1]:
-           return True
-           
